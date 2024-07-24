3eb84cdb3bb90edb4263c233e8309d6704ab2b9f
Andres Del Rio <addelrio11@gmail.com>
2024-07-24 12:08:22 -0400

    start of migration from RapidAPI to Azure
    Bing news does not work on Rapid API anymore, started to migrate to Azure.

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoNewsHeaders = {
-    'X-BingApis-SDK': 'true',
+     /*'X-BingApis-SDK': 'true',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-    'X-RapidAPI-Host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+    'X-RapidAPI-Host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,*/
+    'Ocp-Apim-Subscription-Key': process.env.REACT_APP_AZURE_BING_API_KEY,
   };
 
 
@@ -11,12 +12,15 @@ const createRequest = (url) => ({ url, headers: cryptoNewsHeaders});
 
 export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
-    baseQuery: fetchBaseQuery({ baseUrl:  process.env.REACT_APP_NEWS_API_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl:  process.env.REACT_APP_AZURE_BING_API_KEY }),
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
             query: ({newsCategory, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
         })
     })
 });
+console.log(process.env.REACT_APP_AZURE_BING_API_KEY);
+console.log(process.env.REACT_APP_AZURE_BING_API_ENDPOINT);
 
 export const { useGetCryptoNewsQuery } = cryptoNewsApi;
+
